15b4410433f7070539aaceeed3a25273397a8094
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2026-04-02 18:09:00 +0530

    ImMast added

---
+++ Common/ImMast/V1/show.js
@@ -0,0 +1,16 @@
+import { readFileSync } from "fs";
+import MDBReader from "mdb-reader";
+const dataPath = "\\\\tally\\e\\talwar\\V1\\Data\\2026\\IAM.mdb";
+
+const buffer = readFileSync(dataPath);
+const reader = new MDBReader(buffer);
+const tableName = "ImMast";
+
+// Get table names
+const tableNames = reader.getTableNames();
+// console.log(tableNames); // e.g., ['Users', 'Products']
+
+// Get data from a specific table
+const usersTable = reader.getTable(tableName);
+const data = usersTable.getData();
+console.log(data); // e.g., [{ id: 1, name: 'Alice' }, ...]
