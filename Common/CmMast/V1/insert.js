617aefe91f3e934cf06b3861a0bd8a0da2bd204a
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2026-04-02 17:25:53 +0530

    CmMasters and PSMasters are created in Common

---
+++ Common/CmMast/V1/insert.js
@@ -0,0 +1,19 @@
+import ADODB from 'node-adodb';
+const dataPath = "\\\\tally\\e\\talwar\\V1\\Data\\2026\\IAM.mdb";
+const tableName = "CmMast";
+
+const connection = ADODB.open(
+    "Provider=Microsoft.Jet.OLEDB.4.0;" +
+    `Data Source=${dataPath};` +
+    "Jet OLEDB:Database Password=sekhar;"
+);
+
+async function run() {
+    const res = await connection.execute(
+        `INSERT INTO ${tableName} (PgCode,PgName) VALUES (58,'a1')`
+    );
+
+    console.log(res);
+};
+
+run();
